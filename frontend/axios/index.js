2f58739965a4eec5e5023fa3f0965a975e03b2c9
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-05-27 09:56:24 -0700

    Added axiosWithAuth

--- frontend/axios/index.js
+++ frontend/axios/index.js
@@ -2,6 +2,12 @@
 
 import axios from "axios";
 
-const axiosWithAuth = () => {
-    
-}
+export const axiosWithAuth = () => {
+    const token = localStorage.getItem('token');
+    return axios.create({
+        baseURL: 'http://localhost:9000/api',
+        headers: {
+            authorization: token
+        }
+    });
+};
